98320f458ae2518b7cc5603d81a4e8244144a6b8
Scott Morse <scottmorsedev@gmail.com>
2019-01-31 15:04:21 -0600

    Initial

---
+++ finished/script.js
@@ -0,0 +1,41 @@
+const screen = document.getElementById('screen')
+
+const enterButton = document.getElementById('enter')
+const clearButton = document.getElementById('clear')
+
+const numButtons = document.querySelectorAll('.num-button')
+const opButtons = document.querySelectorAll('.op-button')
+
+function addToScreen(text){
+  screen.innerHTML += text
+}
+
+function clearScreen(){
+  screen.innerHTML = ""
+}
+
+function clickNum(e){
+  const numClicked = e.target.innerHTML
+  addToScreen(numClicked)
+}
+
+function clickOp(e){
+  const opClicked = e.target.innerHTML
+  addToScreen(" " + opClicked + " ")
+}
+
+function clickEnter(){
+  screen.innerHTML = eval(screen.innerHTML)
+}
+
+numButtons.forEach(numButton => {
+  numButton.addEventListener('click',clickNum)
+})
+
+opButtons.forEach(opButton => {
+  opButton.addEventListener('click',clickOp)
+})
+
+clearButton.addEventListener('click',clearScreen)
+
+enterButton.addEventListener('click',clickEnter)
